56f6de46f30ba75818e2a1140ede103efa7c6e0f
MALINAYAGODA <86769332+MALINAYAGODA@users.noreply.github.com>
2021-12-31 23:26:50 +0300

    1.4
    с новым годом

--- MAIN_WINDOW.py
+++ MAIN_WINDOW.py
@@ -34,7 +34,10 @@ def show_up_info(up_info, screen, health=0, time=0, distance=0):
                 i.image = png_files["6_up.png"]
     up_info.draw(screen)
     time_sh = pygame.font.Font(None, 40)
-    text = time_sh.render(f"{time // 60}:{time % 60}", True, (255, 204, 0))
+    if time % 60 < 10:
+        text = time_sh.render(f"{time // 60}:0{time % 60}", True, (255, 204, 0))
+    else:
+        text = time_sh.render(f"{time // 60}:{time % 60}", True, (255, 204, 0))
     screen.blit(text, [170 + indent_x, 5])
 
     distance_sh = pygame.font.Font(None, 30)
@@ -44,7 +47,8 @@ def show_up_info(up_info, screen, health=0, time=0, distance=0):
 
 def show_speed(up_info, screen, speed=0):
     for i in up_info:
-        i.image = png_files[f"speed_{speed}.png"]
+        if speed <= 12:
+            i.image = png_files[f"speed_{speed}.png"]
     up_info.draw(screen)
 
 
@@ -90,7 +94,7 @@ if __name__ == '__main__':
                  'tree_13.png': load_image(f"tree_13.png"), 'tree_14.png': load_image(f"tree_14.png"),
                  'tree_15.png': load_image(f"tree_15.png"), 'tree_16.png': load_image(f"tree_16.png"),
                  'tree_17.png': load_image(f"tree_17.png"), 'tree_18.png': load_image(f"tree_18.png"),
-                 'tree_19.png': load_image(f"tree_19.png"), }
+                 'tree_19.png': load_image(f"tree_19.png")}
     pygame.init()  # создаем окно
     pygame.display.set_caption('Yandex_Race')
     size = width, height = 400 + 162 * 2, 799
@@ -216,6 +220,15 @@ if __name__ == '__main__':
     start_time = time.time()
     end_time = 0
     distance = 2000  # дистанцию которую надо пройти
+    # музыка
+    start_music = pygame.mixer.Sound('music\start_music.mp3')
+    main_music = pygame.mixer.Sound(f'music\music_{random.randint(1, 10)}.mp3')
+    main_music.set_volume(0.5)
+    end_music = pygame.mixer.Sound('music\music_end.mp3')
+    music_boom = pygame.mixer.Sound('music\music_boom.mp3')
+    motor_music = pygame.mixer.Sound('music\motor.mp3')
+    pygame.mixer.Sound.play(main_music)
+    speed_time_limit = 0
     while running:
         if health == 0:
             speed = 0
@@ -231,6 +244,8 @@ if __name__ == '__main__':
             side_road_right.draw(screen)
             main_car_group.draw(screen)
             car_road.draw(screen)
+            tree_group_1.draw(screen)
+            tree_group_2.draw(screen)
 
             font = pygame.font.Font(None, 80)
             text = font.render("GAME OVER", True, (255, 204, 0))
@@ -245,11 +260,18 @@ if __name__ == '__main__':
                             directions['right'] = True
                         elif event.key == pygame.K_a:
                             directions['left'] = True
-                        if event.key == pygame.K_w:  # управление скорости
+                        if event.key == pygame.K_w and speed_time_limit == 0:  # управление скорости
                             speed += 1
+                            pygame.mixer.Sound.play(motor_music)
+                            speed_time_limit = 180
                         if event.key == pygame.K_s:
                             if speed != 3:
                                 speed -= 1
+                        if event.key == pygame.K_z:
+                            pygame.mixer.Sound.stop(main_music)
+                            main_music = pygame.mixer.Sound(f'music\music_{random.randint(1, 10)}.mp3')
+                            main_music.set_volume(0.5)
+                            pygame.mixer.Sound.play(main_music)
                     if event.type == pygame.KEYUP:
                         if event.key == pygame.K_d:
                             directions['right'] = False
@@ -268,6 +290,7 @@ if __name__ == '__main__':
                     car.image = png_files["real_car.png"]
                     what_was = 'real_car.png'
             else:
+                pygame.mixer.Sound.play(start_music)
                 car.rect.y -= 5
                 if car.rect.y <= 500:
                     start = True
@@ -321,6 +344,11 @@ if __name__ == '__main__':
                     blocks_hit_list[0].image = png_files["break_car_road.png"]
                     kol_stol += 1
                     health -= 1
+                    if health == 0:
+                        pygame.mixer.Sound.stop(main_music)
+                        pygame.mixer.Sound.play(end_music)
+                    else:
+                        pygame.mixer.Sound.play(music_boom)
                     sec_neuas = 2 * fps  # бесмертие
                     print(kol_stol)
                     print(blocks_hit_list)
@@ -343,6 +371,8 @@ if __name__ == '__main__':
         else:
             show_up_info(up_info, screen, 6 - health, end_time, distance)
             show_speed(speed_info, screen, 3)
+        if speed_time_limit != 0:
+            speed_time_limit -= 1
         clock.tick(fps)
         pygame.display.flip()
     pygame.quit()
