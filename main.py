985b21b2e09c4a1b9259eabdd12d4493caa11d7d
Vovadoes <dereviannykh.v@gmail.com>
2022-01-07 22:15:15 +0300

    123

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ if __name__ == '__main__':
     size = width, height = 1600, 900
     screen = pygame.display.set_mode(size)
 
-    # starter = Starter(menu, screen, size)
-    starter = Starter(map_display, screen=screen, size=size, save=Save())
+    starter = Starter(menu, screen, size)
+    #starter = Starter(map_display, screen=screen, size=size, save=Save())
 
     while True:
         starter = starter.start()
