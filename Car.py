af7e017568673a00b2ce8ba85d9dc5e6f4b5d313
Vovadoes <dereviannykh.v@gmail.com>
2022-01-11 21:35:31 +0300

    Car

--- Car.py
+++ Car.py
@@ -1,6 +1,5 @@
-
 class ClassCar:
-    def __init__(self, name, k_money = 1):
+    def __init__(self, name, k_money=1):
         self.name = name
         self.k_money = k_money
 
@@ -8,3 +7,16 @@ class ClassCar:
 classes_car = []
 classes_car.append(ClassCar("Обычный"))
 classes_car.append(ClassCar("Премиум", 2))
+
+
+class Car:
+    def __init__(self):
+        self.image_left = None
+        self.image_right = None
+        self.class_car = None
+
+    def load(self):
+        return self
+
+    def save(self):
+        pass
